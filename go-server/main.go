8ef7fe42dc8740f72fe3ba429c7de995b674cbc1
gunjpan <gunjanp@ca.ibm.com>
2017-07-17 12:10:39 -0400

    Intial server scaffolding

---
+++ go-server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "log"
+  "net/http"
+
+  sw "github.com/GoLearning/go-server/models"
+)
+
+func main() {
+  log.Printf("Server started at localhost:8080")
+
+  router := sw.NewRouter()
+
+  log.Fatal(http.ListenAndServe(":8080", router))
+}
---
+++ go-server/models/lot.go
@@ -0,0 +1,38 @@
+package models
+
+import "net/http"
+
+type Lot struct {
+
+}
+
+func CreateLot(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+}
+
+func DeleteLot(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+}
+
+func FindLot(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+}
+
+func ParkCar(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+}
+
+func UnparkCar(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+}
+
+func UpdateLot(w http.ResponseWriter, r *http.Request) {
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusOK)
+}
+
